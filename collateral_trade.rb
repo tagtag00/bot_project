78b1cf058b75c544147565acdedc870bdc61620c
田栗信昭 <>
2018-03-17 16:28:29 +0900

    no message

--- collateral_trade.rb
+++ collateral_trade.rb
@@ -577,7 +577,9 @@ def bollingerTrigger(range = 10)
         row = (value[3]['plus3sigma'] - value[3]["minus3sigma"]) / (value[0]['plus3sigma'] - value[0]["minus3sigma"])
 
         if mid_value[0] > 0 && mid_value[1] > 0
-            if buyres[0][0] < 0 && buyres[0][1] < 0 && buyres[0][2] > 0
+            if buyres[1][0] < 0 && buyres[1][1] < 0 && buyres[1][2] > 0
+                trigger = "buy"
+            elsif buyres[0][0] < 0 && buyres[0][1] < 0 && buyres[0][2] > 0
                 trigger = "buy"
             elsif midres[0] < 0 && midres[1] > 0 && midres[2] > 0
                 trigger = "buy"
@@ -585,7 +587,9 @@ def bollingerTrigger(range = 10)
                 trigger = "sale"
             end
         elsif mid_value[0] < 0 && mid_value[1] < 0
-            if saleres[0][0] > 0 && saleres[0][1] > 0 && saleres[0][2] < 0
+            if saleres[1][0] > 0 && saleres[1][1] > 0 && saleres[1][2] < 0
+                trigger = "sale"
+            elsif saleres[0][0] > 0 && saleres[0][1] > 0 && saleres[0][2] < 0
                 trigger = "sale"
             elsif midres[0] > 0 && midres[1] > 0 && midres[2] < 0
                 trigger = "sale"
