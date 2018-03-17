5912ca4b5fc9bbdc381978fbd0031a7c2bdc02bf
田栗信昭 <>
2018-03-17 14:16:42 +0900

    no message

--- collateral_trade.rb
+++ collateral_trade.rb
@@ -1334,7 +1334,7 @@ loop do
 
             order_list.push(time, result['mid_price'], "stop_order")
 
-        elsif total_collateral['open_position_pnl'] > profit_price && profit_order_status == PROFIT_ORDER_OFF
+        elsif total_collateral['open_position_pnl'] > profit_price * total_position * 100 && profit_order_status == PROFIT_ORDER_OFF
 
             # 未成立取引のキャンセル
             puts child_results = getChildOrders(product_code)
